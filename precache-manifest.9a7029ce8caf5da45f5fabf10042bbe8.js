81c28b7afba5272c27c66a409b1def10678f8617
Vladimir <vladimirplotnikov371@gmail.com>
2021-02-12 22:28:28 +0200

    Updates

---
+++ precache-manifest.9a7029ce8caf5da45f5fabf10042bbe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8279a518d7a9d0c96d2d31333842a017",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "c9dcc1ba4910ecc8cdc9",
+    "url": "/todo/static/css/main.c572f211.chunk.css"
+  },
+  {
+    "revision": "4edd5c7db951aeb0211d",
+    "url": "/todo/static/js/2.5771783a.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/todo/static/js/2.5771783a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9dcc1ba4910ecc8cdc9",
+    "url": "/todo/static/js/main.95000705.chunk.js"
+  },
+  {
+    "revision": "1fa529c815a080658bb3",
+    "url": "/todo/static/js/runtime-main.f4127321.js"
+  }
+]);
